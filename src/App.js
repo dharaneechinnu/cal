3870959e09a88fa9540712afadfe0e61e8075a7f
dharaneedharan <117936700+dharaneechinnu@users.noreply.github.com>
2024-06-26 22:39:08 +0530

    c

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [web3, setWeb3] = useState(null);
   const [account, setAccount] = useState('');
   const [contract, setContract] = useState(null);
-  const contractAddress = '0x8D38b97C8c0F07D5A2d597d812aC698b3C828521'; // Replace with your deployed contract address
+  const contractAddress = '0x76e28Af15D581eAb4ed4c910C9Dd560ADAe4B4AD'; // Replace with your deployed contract address
 
   useEffect(() => {
     const init = async () => {
@@ -19,7 +19,7 @@ function App() {
         const accounts = await web3.eth.getAccounts();
         console.log('Accounts:', accounts);
 
-        const contractInstance = new web3.eth.Contract(CalculatorContract.Abi, contractAddress);
+        const contractInstance = new web3.eth.Contract(CalculatorContract.abi, contractAddress);
         console.log('Contract Instance:', CalculatorContract.Abi);
 
         setWeb3(web3);
@@ -33,20 +33,20 @@ function App() {
     init();
   }, []);
 
-  const handleOperation = async (operation) => {
+  const handleOperation = async() => {
     try {
       if (!contract) {
         console.error("Contract is not initialized");
         return;
       }
-      console.log(`Calling ${operation} method with params:`, sum1, sum2);
-      const receipt = await contract.methods[operation](parseInt(sum1), parseInt(sum2)).send({ from: account });
+      console.log(`Calling  method with params:`, sum1, sum2);
+      const receipt = await contract.method.add(parseInt(sum1), parseInt(sum2)).send({ from: account });
       console.log("Transaction receipt:", receipt);
-      const total = await contract.methods.total().call();
+      const total = await contract.method.total().call();
       setResult(total);
       console.log("Result:", total);
     } catch (error) {
-      console.error(`Error executing ${operation} method: `, error);
+      console.error(`Error executing  method: `, error);
     }
   };
 
@@ -56,10 +56,8 @@ function App() {
       <form onSubmit={(e) => e.preventDefault()}>
         A: <input type="number" className='sum1' value={sum1} onChange={(e) => setsum1(e.target.value)} /><br />
         B: <input type="number" className='sum1' value={sum2} onChange={(e) => setsum2(e.target.value)} /><br />
-        <button onClick={() => handleOperation('add')}>Add</button>
-        <button onClick={() => handleOperation('sub')}>Subtract</button>
-        <button onClick={() => handleOperation('mul')}>Multiply</button>
-        <button onClick={() => handleOperation('div')}>Divide</button>
+        <button onClick={() => handleOperation()}>Add</button>
+       
       </form>
       {result !== null && <h2>Result: {result.toString()}</h2>}
     </div>
